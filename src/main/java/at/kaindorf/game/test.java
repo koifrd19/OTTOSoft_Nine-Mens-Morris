e1ce2d3c7c2f937378ecba046cf29c04a33a9ae6
Franz <koifrd19@htl-kaindorf.at>
2022-03-28 10:22:48 +0200

    string + printf

--- src/main/java/at/kaindorf/game/test.java
+++ src/main/java/at/kaindorf/game/test.java
@@ -6,5 +6,8 @@ public class test {
         System.out.println("otto ist lieb!!");
         System.out.println("GIT is supa");
         System.out.println("Wenn des functoning dann git supa");
+
+        String otto = "otto";
+        System.out.printf("%s, ist wunderbar", otto);
     }
 }
