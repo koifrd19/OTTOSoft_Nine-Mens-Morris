ac0c2398a63e4e9acb3dbdb8bc5253c100fb6650
Allwinger <allotd19@htl-kaindorf.at>
2022-04-07 08:50:41 +0200

    Structure Update and Input

---
+++ src/main/java/at/kaindorf/game/audio/AudioClip.java
@@ -0,0 +1,4 @@
+package at.kaindorf.game.audio;
+
+public abstract class AudioClip {
+}
---
+++ src/main/java/at/kaindorf/game/audio/AudioPlayer.java
@@ -0,0 +1,35 @@
+package at.kaindorf.game.audio;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AudioPlayer {
+
+    private List<AudioClip> audioClips;
+
+    public AudioPlayer() {
+        this.audioClips = new ArrayList<>();
+    }
+
+    public void playMusic(String fileName){
+
+    }
+
+    public Clip getClip(String fileName){
+        final URL soundFile = AudioPlayer.class.getResource("/sound/" + fileName);
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            final Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.setMicrosecondPosition(0);
+            return clip;
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+}
